9ab917887083d36f961d1fdb20d4942f3c33d13f
EeegorL <s2000998@edu.bc.fi>
2022-06-01 00:15:52 +0300

    BSVideo-loop toimii, BSVideon koko ei isompi kuin sivu,  BSCaptioniin ripaus marginia

--- client/src/components/BSVideo.js
+++ client/src/components/BSVideo.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Video from "../assets/video/background-video.mp4";
+import "../components/BSVideo.css"
 
 const BSVideo = () => {
     return (
         <div class="video-background">
             <div class="video-wrap">
-                <div id="video">
-                    <video id="bgvid" autoplay loop muted playsinline>
+                <div id="video" >
+                    <video id="bgvid" autoPlay loop muted playsinline class="videoWidth">
                         <source src={Video} type="video/mp4"></source>
                     </video>
                 </div>
