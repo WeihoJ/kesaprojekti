96348d4c0dc9b6ad471184f10117394f5b9674a3
EeegorL <s2000998@edu.bc.fi>
2022-06-02 11:37:36 +0300

    React-Router-DOM saatu edes jonkinlaiseen alkuun. BSCaptioniin lisätty lyhyt koodipätkä, joka lisää tekstiä linkin painalluksesta. Voi tarvittaessa kommentoida pois tai poistaa.

--- client/src/components/BSCaption.js
+++ client/src/components/BSCaption.js
@@ -1,11 +1,21 @@
 import React from "react";
-
+import { BrowserRouter as Router,Route, Routes, Link} from "react-router-dom";
+import Testi from "./testi";
 const BSCaption = () => {
     return (
         <div class="caption text-center">
             <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h1>
             <a class="btn btn-outline-light btn-lg" href="#course">Lue lisää</a>
+            <Router>
+                        <Routes>
+                            <Route exact path="/testi" element={<Testi/>}>
+                            </Route>
+                        </Routes>
+                        <Link to="/testi">Testi</Link>
+
+                    </Router>
         </div>
+        
     );
 };
 
---
+++ client/src/components/testi.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Testi = () => {
+    return (
+            <h1>Tämä teksti tuotettu React-routerilla</h1>
+            
+    );
+};
+
+export default Testi;
