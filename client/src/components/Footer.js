0dee198c8335923dff673d3b9ac985b305890f2d
Veijo Kasanen <veijok119@gmail.com>
2022-06-07 20:01:27 +0300

    Footer parempi responsiivisuus. Mobiili responsiivisuus kesken.

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./footer.css"
 
 const Footer = (tiedot) => {
     const [nimi, setNimi] = useState("");
@@ -42,14 +41,13 @@ const Footer = (tiedot) => {
     return (
         <footer>
             <div class="container-fluid">
-                <div class="text-center">
+                <div class="text-center FOtsikko">
                     <h1>Ota yhteyttä</h1>
                 </div>
 
                 <div class="row">
                     <div class="col">
                         <div class="footerimg">_</div>
-
                         <ol>
                             <li>
                                 <a class="linkNoDec" href="https://www.google.com/maps/place/Ruostekuja+3,+01610+Vantaa/data=!4m2!3m1!1s0x468df7b1ae8f619b:0xbbcdf0f356902197?sa=X&ved=2ahUKEwjAypuvg4f4AhVaAxAIHSz2DlEQ8gF6BAgZEAE">Ruostekuja 3, Vantaa, 01610</a>
@@ -64,47 +62,21 @@ const Footer = (tiedot) => {
                     </div>
 
                     <div class="col">
-                        
-                    </div>
-                </div>
-            </div>
-
-            <div>
-                <h1 class="centerh1">Ota yhteyttä</h1>
-                <div class="grid">
-                    <div class="griditem gridleft">
-                        <div class="footerimg">_</div>
-
-                        <ol>
-                            <li>
-                                <a class="linkNoDec" href="https://www.google.com/maps/place/Ruostekuja+3,+01610+Vantaa/data=!4m2!3m1!1s0x468df7b1ae8f619b:0xbbcdf0f356902197?sa=X&ved=2ahUKEwjAypuvg4f4AhVaAxAIHSz2DlEQ8gF6BAgZEAE">Ruostekuja 3, Vantaa, 01610</a>
-                            </li>
-                            <li>
-                                <a class="linkNoDec" href="tel:09 14890200">09 14890200</a>
-                            </li>
-                            <li>
-                                <a class="linkNoDec" href="mailto:ruosteinenrauta@hotmail.com">ruosteinenrauta@hotmail.com</a>
-                            </li>
-                        </ol>
-                    </div>
-                    <div>
-                        <div class="griditem gridright">
-                            <div>{msg}</div>
-                            <form onSubmit={tallennaPyynto} class="footerform">
-                                <label><input placeholder="etu- ja sukunimi" class="footerinput" name="nimi" onChange={(e) => setNimi(e.target.value)}></input></label>
-                                <label><input placeholder="puhelinnumero" class="footerinput" name="puhnro" onChange={(e) => setPuhnro(e.target.value)} type="tel"></input></label>
-                                <label><input placeholder="sähköposti" class="footerinput" name="sposti" onChange={(e) => setSposti(e.target.value)} type="email"></input></label>
-                                <label><input placeholder="viesti" class="footerinput" name="viesti" onChange={(e) => setViesti(e.target.value)}></input></label>
-                                <button type="submit" class="formbutton">Lähetä</button>
+                        <div class="row">
+                            <div class="col text-center">{msg}</div>
+                            <form onSubmit={tallennaPyynto} class="formi">
+                                <div class="col"><input placeholder="etu- ja sukunimi" class="footerinput" name="nimi" onChange={(e) => setNimi(e.target.value)}></input></div>
+                                <div class="col"><input placeholder="puhelinnumero" class="footerinput" name="puhnro" onChange={(e) => setPuhnro(e.target.value)} type="tel"></input></div>
+                                <div class="col"><input placeholder="sähköposti" class="footerinput" name="sposti" onChange={(e) => setSposti(e.target.value)} type="email"></input></div>
+                                <div class="col"><input placeholder="viesti" class="footerinput" name="viesti" onChange={(e) => setViesti(e.target.value)}></input></div>
+                                <div class="col"><button type="submit" class="formbutton footerinput">Lähetä</button></div>
                             </form>
                         </div>
                     </div>
                 </div>
-                <div class="copyright">Taitaja &#169; 2022</div>
-
-                <div class="testijono">
-                </div>
             </div>
+
+            <div class="text-center copyright">Taitaja &#169; 2022</div>
         </footer>
 
     );
