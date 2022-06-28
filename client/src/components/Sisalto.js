1ce91231f4d13a538bc99976570ba3a96c1a68ab
EeegorL <s2000998@edu.bc.fi>
2022-06-28 18:43:02 +0300

    Sisältökohtaisia sivuja kivemman näköiseksi, ei mitään tärkeetä, mutta kautta Luojan nimen ne vitun base64-binäärikuvat ei toimi vieläkään ja tää kuumuus siinä sen mukana vielä nii mielenterveys oikeesti rappeutuu ennätysvauhtia

--- client/src/components/Sisalto.js
+++ client/src/components/Sisalto.js
@@ -38,19 +38,35 @@ function Sisalto () {
         EtsiSivu();
 
         return(
-            <div class="margintop ml-5">
-                <p>{nimi}</p>
-                <p>{otsikko}</p>
-                <p>{teemakuvaBin}</p>
-                <img src={teemakuvaBin} alt="Ei vittu toimi legit hyppään kaivoon"/>
-                <img src="data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUA
+            <div class="margintop2 sisalto ml-5" id="tr">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">Sivun nimi</th>
+                        <th scope="col">Otsikko</th>
+                        <th scope="col">URL</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr key={nimi}>
+                        <th scope="row">{nimi}</th>
+                        <td>{otsikko}</td>
+                        <td>localhost:3000/sisalto/{url}</td>
+                    </tr>
+
+                </tbody>
+            </table>
+                <img src={teemakuvaBin} alt="(teemakuva) Ei vittu toimi legit hyppään kaivoon"/>
+                {/* <img src="data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUA
     AAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO
-        9TXL0Y4OHwAAAABJRU5ErkJggg==" alt="Red dot" />
+        9TXL0Y4OHwAAAABJRU5ErkJggg==" alt="Red dot" /> */}
 
             </div>
         )
     };
 
+    
 
   
   
