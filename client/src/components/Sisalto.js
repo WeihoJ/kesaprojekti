ad69b16bc6643a35ead377d31c122617f113cb85
EeegorL <s2000998@edu.bc.fi>
2022-06-29 20:53:53 +0300

    editori voi muokata sivuja kirjauneena, tosin muutoksia ei voi tallentaa eikä sivuilla ole paljoa muokattavaakaan, mutta ei tässä helteessä jaksa edes hengittää

--- client/src/components/Sisalto.js
+++ client/src/components/Sisalto.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import { useParams } from 'react-router-dom';
 import axios from "axios";
 
@@ -7,11 +7,29 @@ function Sisalto () {
     const [url, setUrl] = useState("");
     const [otsikko, setOtsikko] = useState("");
     const [teemakuvaBin, setTeemakuvaBin] = useState("");
-    
+    const [loggedUserRole, setLoggedUserRole] = useState("");
     const sivunUrl= useParams().url;
     axios.defaults.withCredentials = true;
 
     const EtsiSivu = async () => {
+        useEffect(() => {
+            axios
+                .get("http://localhost:3001/api/checklogin")
+                .then((response) => {
+                    // console.log(response.data.loggedIn);
+                    if (response.data.loggedIn) {
+                        setLoggedUserRole(response.data.userRole);
+                    } else {
+                        setLoggedUserRole("");
+                    }
+                })
+                .catch((error) =>
+                    console.log(
+                        "Palvelin ei päällä tai ei anna vastausta api pyyntöön" +
+                            error
+                    )
+                );
+        }, []);
         
             try {
                 const sivutieto = await axios.get(
@@ -39,7 +57,28 @@ function Sisalto () {
 
         return(
             <div class="margintop2 sisalto ml-5" id="tr">
-            <table class="table table-striped">
+            {loggedUserRole=="editor" ? (<div><table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">Sivun nimi</th>
+                        <th scope="col">Otsikko</th>
+                        <th scope="col">URL</th>
+                        <th scope="col"><button>Tallenna muutokset</button></th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr key={nimi}>
+                        <th scope="row" contentEditable="true">{nimi}</th>
+                        <td contentEditable="true">{otsikko}</td>
+                        <td>localhost:3000/sisalto/{url}</td>
+                    </tr>
+                </tbody>
+            </table>
+
+                <img src={teemakuvaBin} alt="(teemakuva) Ei vittu toimi legit hyppään kaivoon"/></div>)
+                :
+                (<div><table class="table table-striped">
                 <thead>
                     <tr>
                         <th scope="col">Sivun nimi</th>
@@ -57,7 +96,8 @@ function Sisalto () {
 
                 </tbody>
             </table>
-                <img src={teemakuvaBin} alt="(teemakuva) Ei vittu toimi legit hyppään kaivoon"/>
+
+                <img src={teemakuvaBin} alt="(teemakuva) Ei vittu toimi legit hyppään kaivoon"/></div>)}
                 {/* <img src="data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUA
     AAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO
         9TXL0Y4OHwAAAABJRU5ErkJggg==" alt="Red dot" /> */}
